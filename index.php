1f7d072e589dfb9a8bdf4e45f9cf63526eef3f48
NastyaRub02 <165319232+NastyaRub02@users.noreply.github.com>
2024-08-01 20:12:05 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,433 @@
+<?php
+    session_start();
+    $pageTitle = "Главная";
+    include('./inc/header.php');
+    include('./inc/welcome.php');
+?>
+
+<section class="cards">
+    <div class="container">
+        <div class="cards_over">
+            <div class="card card_1">
+                <div class="card_text">
+                    <h3 class="card_text_title">
+                    Мы рады приветствовать вас на этой платформе, созданной для всех любителей аниме и японской культуры. 
+                    </h3>
+                    <div class="card_text_p">
+                    Здесь вы сможете открыть для себя удивительные миры аниме и найти новых друзей, которые разделяют вашу страсть к этому искусству.
+                    </div>
+                </div>
+                <div class="card_img">
+                <img src="./img/home/cards/card_photo_1.png" alt="card_photo">
+                </div>
+            </div>
+            <div class="card card_2">
+                <div class="card_text">
+                    <h3 class="card_text_title">
+                    Мы рады приветствовать вас на этой платформе, созданной для всех любителей аниме и японской культуры. 
+                    </h3>
+                    <div class="card_text_p">
+                    Здесь вы сможете открыть для себя удивительные миры аниме и найти новых друзей, которые разделяют вашу страсть к этому искусству.
+                    </div>
+                </div>
+                <div class="card_img">
+                <img src="./img/home/cards/card_photo_2.png" alt="card_photo">
+                </div>
+            </div>
+            <div class="card card_3">
+                <div class="card_text">
+                    <h3 class="card_text_title">
+                    Мы рады приветствовать вас на этой платформе, созданной для всех любителей аниме и японской культуры. 
+                    </h3>
+                    <div class="card_text_p">
+                    Здесь вы сможете открыть для себя удивительные миры аниме и найти новых друзей, которые разделяют вашу страсть к этому искусству.
+                    </div>
+                </div>
+                <div class="card_img">
+                <img src="./img/home/cards/card_photo_3.png" alt="card_photo">
+                </div>
+            </div>
+            <div class="card card_4">
+                <div class="card_text">
+                    <h3 class="card_text_title">
+                    Мы рады приветствовать вас на этой платформе, созданной для всех любителей аниме и японской культуры. 
+                    </h3>
+                    <div class="card_text_p">
+                    Здесь вы сможете открыть для себя удивительные миры аниме и найти новых друзей, которые разделяют вашу страсть к этому искусству.
+                    </div>
+                </div>
+                <div class="card_img">
+                <img src="./img/home/cards/card_photo_4.png" alt="card_photo">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="sakuga_video">
+    <div class="container">
+        <div class="title_text_sakuga_video">
+            <h2 class="title">
+            Сакуга
+            </h2>
+            <div class="slider_2">
+                <div class="slider_item sakuga_video">
+            <div class="sakuga_video">
+            <iframe class="sakuga_video_iframe" src="https://www.youtube.com/embed/18ybhFypQOs?si=AnJcZaQap-jkHM9o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+                </div>
+                <div class="slider_item sakuga_video">
+            <div class="sakuga_video">
+            <iframe class="sakuga_video_iframe" src="https://www.youtube.com/embed/18ybhFypQOs?si=AnJcZaQap-jkHM9o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+                </div>
+                <div class="slider_item sakuga_video">
+            <div class="sakuga_video">
+            <iframe class="sakuga_video_iframe" src="https://www.youtube.com/embed/18ybhFypQOs?si=AnJcZaQap-jkHM9o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="reviews">
+    <div class="container">
+        <div class="title_rating_reviews">
+            <h2 class="title">
+            Рецензии
+            </h2>
+        </div>
+        <div class="slider">
+                <div class="slider_item">
+                    <a href=""><div class="card-slider">
+                        <div class="title_subtitle_text_img-slider">
+                        <div class="title_img-slider"><img src="./img/slider/ava1.png" alt="slider_img_ava"></div>
+                        <div class="title_subtitle_text-slider">
+                        <h3 class="title-text_slider">
+                        Naryto
+                        </h3>
+                        <h2 class="subtitle-text_slider">
+                        Daniel visser
+                        </h2>
+                        </div>
+                        </div>
+
+                        <div class="text_review-slider">
+                        <p class="text_review-slider_p">
+                        Наруто это самое потрясающее аниме !!! Обалденный мульт. История о прикольном пацане Наруте, который в каждой серии усовершенствует свои силы и попадает в самые разнообразные приключения, в этом мультфильме полно трогательных, и очень смешных моментов. Полно разных героев которые обладают собственными, магическими техниками. В каждой серии раскрываются тайны и появляются новые еще более таинственней.
+                        Главные герои это три генина деревни конохи, которые состоят в одной команде: Наруто, Сакура, Саске - лучший друг Наруто, и капитан команды Какаши. Это очень запутанная и очень...
+                        </p>
+                    </div>
+                    </div></a>
+                </div>
+                <div class="slider_item">
+                <a href=""><div class="card-slider">
+                        <div class="title_subtitle_text_img-slider">
+                        <div class="title_img-slider"><img src="./img/slider/ava1.png" alt="slider_img_ava"></div>
+                        <div class="title_subtitle_text-slider">
+                        <h3 class="title-text_slider">
+                        Naryto
+                        </h3>
+                        <h2 class="subtitle-text_slider">
+                        Daniel visser
+                        </h2>
+                        </div>
+                        </div>
+
+                        <div class="text_review-slider">
+                        <p class="text_review-slider_p">
+                        Наруто это самое потрясающее аниме !!! Обалденный мульт. История о прикольном пацане Наруте, который в каждой серии усовершенствует свои силы и попадает в самые разнообразные приключения, в этом мультфильме полно трогательных, и очень смешных моментов. Полно разных героев которые обладают собственными, магическими техниками. В каждой серии раскрываются тайны и появляются новые еще более таинственней.
+                        Главные герои это три генина деревни конохи, которые состоят в одной команде: Наруто, Сакура, Саске - лучший друг Наруто, и капитан команды Какаши. Это очень запутанная и очень...
+                        </p>
+                    </div>
+                    </div></a>
+                </div>
+                <div class="slider_item">
+                <a href=""><div class="card-slider">
+                        <div class="title_subtitle_text_img-slider">
+                        <div class="title_img-slider"><img src="./img/slider/ava1.png" alt="slider_img_ava"></div>
+                        <div class="title_subtitle_text-slider">
+                        <h3 class="title-text_slider">
+                        Naryto
+                        </h3>
+                        <h2 class="subtitle-text_slider">
+                        Daniel visser
+                        </h2>
+                        </div>
+                        </div>
+
+                        <div class="text_review-slider">
+                        <p class="text_review-slider_p">
+                        Наруто это самое потрясающее аниме !!! Обалденный мульт. История о прикольном пацане Наруте, который в каждой серии усовершенствует свои силы и попадает в самые разнообразные приключения, в этом мультфильме полно трогательных, и очень смешных моментов. Полно разных героев которые обладают собственными, магическими техниками. В каждой серии раскрываются тайны и появляются новые еще более таинственней.
+                        Главные герои это три генина деревни конохи, которые состоят в одной команде: Наруто, Сакура, Саске - лучший друг Наруто, и капитан команды Какаши. Это очень запутанная и очень...
+                        </p>
+                    </div>
+                    </div></a>
+                </div>
+                <div class="slider_item">
+                <a href=""><div class="card-slider">
+                        <div class="title_subtitle_text_img-slider">
+                        <div class="title_img-slider"><img src="./img/slider/ava1.png" alt="slider_img_ava"></div>
+                        <div class="title_subtitle_text-slider">
+                        <h3 class="title-text_slider">
+                        Naryto
+                        </h3>
+                        <h2 class="subtitle-text_slider">
+                        Daniel visser
+                        </h2>
+                        </div>
+                        </div>
+
+                        <div class="text_review-slider">
+                        <p class="text_review-slider_p">
+                        Наруто это самое потрясающее аниме !!! Обалденный мульт. История о прикольном пацане Наруте, который в каждой серии усовершенствует свои силы и попадает в самые разнообразные приключения, в этом мультфильме полно трогательных, и очень смешных моментов. Полно разных героев которые обладают собственными, магическими техниками. В каждой серии раскрываются тайны и появляются новые еще более таинственней.
+                        Главные герои это три генина деревни конохи, которые состоят в одной команде: Наруто, Сакура, Саске - лучший друг Наруто, и капитан команды Какаши. Это очень запутанная и очень...
+                        </p>
+                    </div>
+                    </div></a>
+                </div>
+            </div>
+    </div>
+</section>
+
+<section class="rating">
+    <div class="container">
+        <div class="title_rating_over">
+            <h2 class="title">
+            рейтинг
+            </h2>
+            <div class="rating_over">
+            <button class="accordion">Приключения</button>
+                <div class="panel">
+                <div class="section_cards_accordion-panel-over">
+                <!-- НАЧАЛО КАРТОЧЕК -->
+                <section class="section_cards_anime-over">
+    <div class="container">
+    <div class="card-anime-display_flex">
+        <!-- Сама карточка card_anime-over -->
+        <a href="#"><div class="card_anime-over">
+            <!-- Полоса рейтинга -->
+            <div class="card_anime_rating-over">
+                <div class="card_anime_rating">
+                    <!-- Сюда код полосы -->
+                </div>
+                <div class="card_anime_rating-text">
+                    <!-- Сюда балл рейтинга -->
+                    <p>8.1</p>
+                </div>
+            </div>
+            <!-- название,жанр и звездочный рейтинг -->
+             <div class="anime_name_genre_rating2-over">
+                <!-- название и жанр -->
+                 <div class="anime_name_genre-over">
+                    <h2 class="anime_rating2_genre_h3">
+                        <!-- Название аниме -->
+                        Иная
+                    </h2>
+                    <p class="anime_rating2_genre_p">
+                        <!-- Жанр аниме -->
+                        хоррор
+                    </p>
+                 </div>
+                 <!-- звездочный рейтинг -->
+                  <div class="card_anime_rating2_text-over">
+                    <div class="card_anime_rating2">
+                        <!-- Сюда код полосы -->
+                        <img src="../img/anime/Star.svg" alt="Star">
+                    </div>
+                    <div class="card_anime_rating2-text">
+                        <!-- Сюда балл рейтинга -->
+                        <p>8.1</p>
+                    </div>
+                </div>
+             </div>
+        </div></a>
+        <!-- конец карточки -->
+        <a href="#"><div class="card_anime-over">
+            <!-- Полоса рейтинга -->
+            <div class="card_anime_rating-over">
+                <div class="card_anime_rating">
+                    <!-- Сюда код полосы -->
+                </div>
+                <div class="card_anime_rating-text">
+                    <!-- Сюда балл рейтинга -->
+                    <p>8.1</p>
+                </div>
+            </div>
+            <!-- название,жанр и звездочный рейтинг -->
+             <div class="anime_name_genre_rating2-over">
+                <!-- название и жанр -->
+                 <div class="anime_name_genre-over">
+                    <h2 class="anime_rating2_genre_h3">
+                        <!-- Название аниме -->
+                        Иная
+                    </h2>
+                    <p class="anime_rating2_genre_p">
+                        <!-- Жанр аниме -->
+                        хоррор
+                    </p>
+                 </div>
+                 <!-- звездочный рейтинг -->
+                  <div class="card_anime_rating2_text-over">
+                    <div class="card_anime_rating2">
+                        <!-- Сюда код полосы -->
+                        <img src="../img/anime/Star.svg" alt="Star">
+                    </div>
+                    <div class="card_anime_rating2-text">
+                        <!-- Сюда балл рейтинга -->
+                        <p>8.1</p>
+                    </div>
+                </div>
+             </div>
+        </div></a>
+        <a href="#"><div class="card_anime-over">
+            <!-- Полоса рейтинга -->
+            <div class="card_anime_rating-over">
+                <div class="card_anime_rating">
+                    <!-- Сюда код полосы -->
+                </div>
+                <div class="card_anime_rating-text">
+                    <!-- Сюда балл рейтинга -->
+                    <p>8.1</p>
+                </div>
+            </div>
+            <!-- название,жанр и звездочный рейтинг -->
+             <div class="anime_name_genre_rating2-over">
+                <!-- название и жанр -->
+                 <div class="anime_name_genre-over">
+                    <h2 class="anime_rating2_genre_h3">
+                        <!-- Название аниме -->
+                        Иная
+                    </h2>
+                    <p class="anime_rating2_genre_p">
+                        <!-- Жанр аниме -->
+                        хоррор
+                    </p>
+                 </div>
+                 <!-- звездочный рейтинг -->
+                  <div class="card_anime_rating2_text-over">
+                    <div class="card_anime_rating2">
+                        <!-- Сюда код полосы -->
+                        <img src="../img/anime/Star.svg" alt="Star">
+                    </div>
+                    <div class="card_anime_rating2-text">
+                        <!-- Сюда балл рейтинга -->
+                        <p>8.1</p>
+                    </div>
+                </div>
+             </div>
+        </div></a>
+        </div>
+    </div>
+</section>
+                <!-- КОНЕЦ КАРТОЧЕК -->
+                </div>
+                </div>
+
+                <button class="accordion">Боевик</button>
+                <div class="panel">
+                <div class="section_cards_accordion-panel-over">
+                <!-- НАЧАЛО КАРТОЧЕК -->
+
+                <!-- КОНЕЦ КАРТОЧЕК -->
+                </div>
+                </div>
+
+                <button class="accordion">Комедия</button>
+                <div class="panel">
+                <div class="section_cards_accordion-panel-over">
+                <!-- НАЧАЛО КАРТОЧЕК -->
+
+                <!-- КОНЕЦ КАРТОЧЕК -->
+                </div>
+                </div>
+
+                <button class="accordion">Повседневность</button>
+                <div class="panel">
+                <div class="section_cards_accordion-panel-over">
+                <!-- НАЧАЛО КАРТОЧЕК -->
+
+                <!-- КОНЕЦ КАРТОЧЕК -->
+                </div>
+                </div>
+
+                <button class="accordion">Романтика</button>
+                <div class="panel">
+                <div class="section_cards_accordion-panel-over">
+                <!-- НАЧАЛО КАРТОЧЕК -->
+
+                <!-- КОНЕЦ КАРТОЧЕК -->
+                </div>
+                </div>
+
+                <button class="accordion">Драма</button>
+                <div class="panel">
+                <div class="section_cards_accordion-panel-over">
+                <!-- НАЧАЛО КАРТОЧЕК -->
+
+                <!-- КОНЕЦ КАРТОЧЕК -->
+                </div>
+                </div>
+
+                <button class="accordion">Фантастика</button>
+                <div class="panel">
+                <div class="section_cards_accordion-panel-over">
+                <!-- НАЧАЛО КАРТОЧЕК -->
+
+                <!-- КОНЕЦ КАРТОЧЕК -->
+                </div>
+                </div>
+
+                <button class="accordion">Фэнтези</button>
+                <div class="panel">
+                <div class="section_cards_accordion-panel-over">
+                <!-- НАЧАЛО КАРТОЧЕК -->
+
+                <!-- КОНЕЦ КАРТОЧЕК -->
+                </div>
+                </div>
+
+                <button class="accordion">Мистика</button>
+                <div class="panel">
+                <div class="section_cards_accordion-panel-over">
+                <!-- НАЧАЛО КАРТОЧЕК -->
+
+                <!-- КОНЕЦ КАРТОЧЕК -->
+                </div>
+                </div>
+
+                <button class="accordion">Детектив</button>
+                <div class="panel">
+                <div class="section_cards_accordion-panel-over">
+                <!-- НАЧАЛО КАРТОЧЕК -->
+
+                <!-- КОНЕЦ КАРТОЧЕК -->
+                </div>
+                </div>
+
+                <button class="accordion">Триллер</button>
+                <div class="panel">
+                <div class="section_cards_accordion-panel-over">
+                <!-- НАЧАЛО КАРТОЧЕК -->
+
+                <!-- КОНЕЦ КАРТОЧЕК -->
+                </div>
+                </div>
+
+                <button class="accordion">Психология</button>
+                <div class="panel">
+                <div class="section_cards_accordion-panel-over">
+                <!-- НАЧАЛО КАРТОЧЕК -->
+
+                <!-- КОНЕЦ КАРТОЧЕК -->
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+    include('./inc/footer.php');
+?>
