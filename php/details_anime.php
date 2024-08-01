0acc872610d5ef11b51c4ed98c87fa9739782206
NastyaRub02 <165319232+NastyaRub02@users.noreply.github.com>
2024-08-01 20:15:25 +0300

    Create details_anime.php

---
+++ php/details_anime.php
@@ -0,0 +1,217 @@
+<?php
+    session_start();
+    $pageTitle = "Аниме";
+    include('../inc/header.php');
+    include('../inc/welcome.php');
+?>
+
+<!-- РЕЙТИНГ ПОЛОСА -->
+<section class="rating_anime-over">
+    <div class="container">
+
+    </div>
+</section>
+
+<!-- РЕЙТИНГ ЗВЕЗДОЧНЫЙ -->
+<section class="rating_star_anime-over">
+    <div class="container">
+
+    </div>
+</section>
+
+<!-- Текстовая информация об аниме -->
+<section class="text_anime_details-over">
+    <div class="container">
+        <p class="text_anime_details_p">Коити Сакакибара был обычным подростком без особых заслуг и достижений. Его мама рано умерла, а отец все время проводил в дальних экспедициях. Без родительского присмотра и хороших условий проживания у парня развилась болезнь легких. Он начал чахнуть в грязном и загазованном Токио. Для более комфортной жизни мальчику пришлось переехать в провинциальный городок Ёмияма, где жила родня его матери.</p>
+        <p class="text_anime_details_p">Тётушка Рэйко начала говорить племяннику, что ему лучше не высовываться в новой школе и не отрываться от коллектива. Коити был удивлен таким наставлениям, но его это устраивало. Однако, попав в школу, мальчик почувствовал, что в его новом девятом «В» классе очень странная, давящая и нездоровая обстановка.</p>
+        <p class="text_anime_details_p">Более 20 лет назад в этом классе училась добрая и общительная девочка по имени Мисаки. Она погибла при неизвестных обстоятельствах, и подробности тех событий до сих пор никому неизвестны. Почему-то все вокруг отрицали ее смерть, и с тех самых пор последнее место левого ряда в 9-м «В» классе остается незанятым. Однако столичный гость не знал этой истории и первым делом поприветствовал тихую девочку с повязкой на глазу. Это вызвало шок у окружающих, после чего они стали подозрительно относиться к чужаку. Коити, пытаясь понять причины происходящего, узнал от своей новой знакомой, что все дело в том, что её зовут Мэй Мисаки!</p>
+    </div>
+</section>
+
+<!-- ГАЛЛЕРЕЯ -->
+<section class="gallery_anime_details-over">
+    <div class="container">
+        <h2 class="title">
+        Галлерея
+        </h2>
+        <div class="block_gallery_anime-over block_1_gallery_anime-over">
+            <div class="block_gallery block_1_gallery">
+            </div>
+            <div class="block_gallery block_2_gallery">
+            </div>
+        </div>
+        <div class="block_gallery_anime-over block_2_gallery_anime-over">
+            <div class="block_gallery block_2_gallery">
+            </div>
+            <div class="block_gallery block_1_gallery">
+            </div>
+        </div>
+        <div class="block_gallery_anime-over block_3_gallery_anime-over">
+            <div class="block_gallery block_3_gallery">
+            </div>
+            <div class="block_gallery block_3_gallery">
+            </div>
+            <div class="block_gallery block_3_gallery">
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- РЕЦЕНЗИИ -->
+<section class="reviews">
+    <div class="container">
+        <div class="title_rating_reviews">
+            <h2 class="title">
+            Рецензии
+            </h2>
+        </div>
+        <div class="slider">
+                <div class="slider_item">
+                    <a href=""><div class="card-slider">
+                        <div class="title_subtitle_text_img-slider">
+                        <div class="title_img-slider"><img src="../img/slider/ava1.png" alt="slider_img_ava"></div>
+                        <div class="title_subtitle_text-slider">
+                        <h3 class="title-text_slider">
+                        Naryto
+                        </h3>
+                        <h2 class="subtitle-text_slider">
+                        Daniel visser
+                        </h2>
+                        </div>
+                        </div>
+
+                        <div class="text_review-slider">
+                        <p class="text_review-slider_p">
+                        Наруто это самое потрясающее аниме !!! Обалденный мульт. История о прикольном пацане Наруте, который в каждой серии усовершенствует свои силы и попадает в самые разнообразные приключения, в этом мультфильме полно трогательных, и очень смешных моментов. Полно разных героев которые обладают собственными, магическими техниками. В каждой серии раскрываются тайны и появляются новые еще более таинственней.
+                        Главные герои это три генина деревни конохи, которые состоят в одной команде: Наруто, Сакура, Саске - лучший друг Наруто, и капитан команды Какаши. Это очень запутанная и очень...
+                        </p>
+                    </div>
+                    </div></a>
+                </div>
+                <div class="slider_item">
+                <a href=""><div class="card-slider">
+                        <div class="title_subtitle_text_img-slider">
+                        <div class="title_img-slider"><img src="../img/slider/ava1.png" alt="slider_img_ava"></div>
+                        <div class="title_subtitle_text-slider">
+                        <h3 class="title-text_slider">
+                        Naryto
+                        </h3>
+                        <h2 class="subtitle-text_slider">
+                        Daniel visser
+                        </h2>
+                        </div>
+                        </div>
+
+                        <div class="text_review-slider">
+                        <p class="text_review-slider_p">
+                        Наруто это самое потрясающее аниме !!! Обалденный мульт. История о прикольном пацане Наруте, который в каждой серии усовершенствует свои силы и попадает в самые разнообразные приключения, в этом мультфильме полно трогательных, и очень смешных моментов. Полно разных героев которые обладают собственными, магическими техниками. В каждой серии раскрываются тайны и появляются новые еще более таинственней.
+                        Главные герои это три генина деревни конохи, которые состоят в одной команде: Наруто, Сакура, Саске - лучший друг Наруто, и капитан команды Какаши. Это очень запутанная и очень...
+                        </p>
+                    </div>
+                    </div></a>
+                </div>
+                <div class="slider_item">
+                <a href=""><div class="card-slider">
+                        <div class="title_subtitle_text_img-slider">
+                        <div class="title_img-slider"><img src="../img/slider/ava1.png" alt="slider_img_ava"></div>
+                        <div class="title_subtitle_text-slider">
+                        <h3 class="title-text_slider">
+                        Naryto
+                        </h3>
+                        <h2 class="subtitle-text_slider">
+                        Daniel visser
+                        </h2>
+                        </div>
+                        </div>
+
+                        <div class="text_review-slider">
+                        <p class="text_review-slider_p">
+                        Наруто это самое потрясающее аниме !!! Обалденный мульт. История о прикольном пацане Наруте, который в каждой серии усовершенствует свои силы и попадает в самые разнообразные приключения, в этом мультфильме полно трогательных, и очень смешных моментов. Полно разных героев которые обладают собственными, магическими техниками. В каждой серии раскрываются тайны и появляются новые еще более таинственней.
+                        Главные герои это три генина деревни конохи, которые состоят в одной команде: Наруто, Сакура, Саске - лучший друг Наруто, и капитан команды Какаши. Это очень запутанная и очень...
+                        </p>
+                    </div>
+                    </div></a>
+                </div>
+                <div class="slider_item">
+                <a href=""><div class="card-slider">
+                        <div class="title_subtitle_text_img-slider">
+                        <div class="title_img-slider"><img src="../img/slider/ava1.png" alt="slider_img_ava"></div>
+                        <div class="title_subtitle_text-slider">
+                        <h3 class="title-text_slider">
+                        Naryto
+                        </h3>
+                        <h2 class="subtitle-text_slider">
+                        Daniel visser
+                        </h2>
+                        </div>
+                        </div>
+
+                        <div class="text_review-slider">
+                        <p class="text_review-slider_p">
+                        Наруто это самое потрясающее аниме !!! Обалденный мульт. История о прикольном пацане Наруте, который в каждой серии усовершенствует свои силы и попадает в самые разнообразные приключения, в этом мультфильме полно трогательных, и очень смешных моментов. Полно разных героев которые обладают собственными, магическими техниками. В каждой серии раскрываются тайны и появляются новые еще более таинственней.
+                        Главные герои это три генина деревни конохи, которые состоят в одной команде: Наруто, Сакура, Саске - лучший друг Наруто, и капитан команды Какаши. Это очень запутанная и очень...
+                        </p>
+                    </div>
+                    </div></a>
+                </div>
+            </div>
+    </div>
+</section>
+
+<!-- ВЕДУЩИЕ ПЕРСОНАЖИ -->
+<section class="leading_characters_anime-over-section">
+    <div class="container">
+    <h2 class="title">
+    ведущие персонажи
+    </h2>
+    <!-- Основа карточек -->
+    <div class="leading_characters_anime-over">
+        <!-- Сама карточка -->
+        <div class="leading_characters_anime-text-over">
+            <div class="leading_characters_anime-img"></div>
+            <a href="#"><p class="leading_characters_anime-p">Аки Мацуи</p></a>
+        </div>
+        <!--  -->
+        <div class="leading_characters_anime-text-over">
+            <div class="leading_characters_anime-img"></div>
+            <a href="#"><p class="leading_characters_anime-p">Аки Мацуи</p></a>
+        </div>
+        <div class="leading_characters_anime-text-over">
+            <div class="leading_characters_anime-img"></div>
+            <a href="#"><p class="leading_characters_anime-p">Аки Мацуи</p></a>
+        </div>
+        <div class="leading_characters_anime-text-over">
+            <div class="leading_characters_anime-img"></div>
+            <a href="#"><p class="leading_characters_anime-p">Аки Мацуи</p></a>
+        </div>
+        <div class="leading_characters_anime-text-over">
+            <div class="leading_characters_anime-img"></div>
+            <a href="#"><p class="leading_characters_anime-p">Аки Мацуи</p></a>
+        </div>
+        <div class="leading_characters_anime-text-over">
+            <div class="leading_characters_anime-img"></div>
+            <a href="#"><p class="leading_characters_anime-p">Аки Мацуи</p></a>
+        </div>
+        <div class="leading_characters_anime-text-over">
+            <div class="leading_characters_anime-img"></div>
+            <a href="#"><p class="leading_characters_anime-p">Аки Мацуи</p></a>
+        </div>
+        <!--  -->
+    </div>
+    </div>
+</section>
+
+<!-- САКУГА -->
+<section class="sakuga_video_anime_details-over">
+    <div class="container">
+    <h2 class="title">
+            Сакуга
+    </h2>
+    <div class="sakuga_video-anime_details">
+    <iframe src="https://www.youtube.com/embed/NoYh7IJhfoE?si=iFWJh6OVpo-z4wjs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+    </div>
+</section>
+<?php
+    include('../inc/footer.php');
+?>
